eaa5f4bb3baa8a4a68245b938c9838155c186ad6
Zheng Zhun Chen <milk8248@gmail.com>
2018-03-30 17:12:53 +0800

    改IP
    改IP

--- app/src/main/java/com/example/lambert/hemodialysis/app/AppConfig.java
+++ app/src/main/java/com/example/lambert/hemodialysis/app/AppConfig.java
@@ -6,8 +6,8 @@ package com.example.lambert.hemodialysis.app;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://140.96.98.66:8082/login";
+    public static String URL_LOGIN = "http://140.96.98.42:8082/login";
 
     // Server user register url
-    public static String URL_REGISTER = "http://140.96.98.66:8082/register";
+    public static String URL_REGISTER = "http://140.96.98.42:8082/register";
 }
